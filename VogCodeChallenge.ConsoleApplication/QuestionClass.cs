17d34ba04fd6c907dd4fa50363c028a922478940
VimalGowda9 <vimalcomp9@gmail.com>
2021-12-13 15:25:11 +0530

    Question 7: Console Application - Iterate through a list without using ForEach/For loops.

---
+++ VogCodeChallenge.ConsoleApplication/QuestionClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VogCodeChallenge.ConsoleApplication
+{
+    /// <summary>
+    /// Question Class
+    /// </summary>
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John"
+        };
+
+        /// <summary>
+        /// Method to iterate through NameList and get the list of Names
+        /// </summary>
+        /// <returns>list of string</returns>
+        public static List<string> GetListOfNames()
+        {
+            var names = new List<string>();
+            int cnt = 0;
+
+            while(cnt < NamesList.Count)
+            {
+                names.Add(NamesList.ElementAt(cnt));
+                cnt++;
+            }
+            
+            return names;
+        }
+    }
+}
---
+++ VogCodeChallenge.ConsoleApplication/VogCode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.ConsoleApplication
+{
+    /// <summary>
+    /// VogCode class
+    /// </summary>
+    public class VogCode
+    {
+        /// <summary>
+        /// Main class
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Please find below the list of Valid Names:");
+
+            var nameList = GetNames();
+
+            if(nameList != null)
+            {
+                string result = string.Join(Environment.NewLine, nameList);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Sorry for the inconvenience. At present, no Valid Names are available in the System");
+            }             
+        }
+
+        /// <summary>
+        /// Method to get list of names from QuestionClass
+        /// </summary>
+        /// <returns>List of names</returns>
+        private static List<string> GetNames()
+        {
+            return QuestionClass.GetListOfNames();            
+        }
+    }
+}
